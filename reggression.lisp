74a932ab84f8bcc459a5f4dd4acae3cbc2e33340
Tomas Zellerin <zellerin@gmail.com>
2018-04-01 20:03:56 +0200

    Fix linear regression.

--- reggression.lisp
+++ reggression.lisp
@@ -33,13 +33,14 @@
 
 (defvar *dy* (linear-combination 1s0 (times *more-x* *test-A*)
 				 -1s0 *y*))
+
 (defun linear-regression-iteration (y A x sigma rho)
   "Iteration step; modifies A."
   (let ((dy (linear-combination 1s0 (times x A)
 				-1s0 y)))
     (values
      dy
-     (linear-combination rho A sigma (times-transposed x dy))
+     (linear-combination rho A sigma (times-transposed x dy ))
      *true-a*
      (times-transposed dy dy))))
 
@@ -77,14 +78,24 @@
 
 (defun logistic-regression-iteration (y A x sigma rho)
   "Iteration step; modifies A."
-  (let ((dy (linear-combination 1s0 (apply-fn #'float-sigma
-					      (times x A))
+  (let ((dy (linear-combination 1s0
+				(apply-fn #'float-sigma
+					  (times x A))
 				-1s0 y)))
     (values
      dy
      (linear-combination rho A sigma
-			 (times-transposed x (apply-fn2 #'float-sigma dy y)))
+			 (times-transposed x (apply-fn2 #'float-dsigma dy y)))
      *true-a*
      (times-transposed dy dy))))
 
 
+(defun check-linear (count)
+  (let ((*test-A* (make-random-array 3 1 1s0)))
+    (dotimes (i count (linear-regression-iteration *y* *test-A* *more-x* -1s-4 1s0))
+      (linear-regression-iteration *y* *test-A* *more-x* -1s-4 1s0))))
+
+(defun copy-array (a)
+  (make-array (array-dimensions a)
+	      :element-type (array-element-type a)
+	      :initial-contents a))
