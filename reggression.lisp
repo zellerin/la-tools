02a7ecd06f1e9dbd026a2f6011e7df8e6341d313
Tomas Zellerin <zellerin@gmail.com>
2018-04-01 17:03:02 +0200

    Both logistic and linear regression do work.

--- reggression.lisp
+++ reggression.lisp
@@ -21,10 +21,8 @@
 
 (defvar *dy* (linear-combination 1s0 (times *more-x* *test-A*)
 				 -1s0 *y*))
-
-(defvar *dA* (times-transposed *more-x* *dy*))
-
-(defun advance (y A x sigma rho)
+(defun linear-regression-iteration (y A x sigma rho)
+  "Iteration step; modifies A."
   (let ((dy (linear-combination 1s0 (times x A)
 				-1s0 y)))
     (values
@@ -32,3 +30,49 @@
      (linear-combination rho A sigma (times-transposed x dy))
      *true-a*
      (times-transposed dy dy))))
+
+(defun apply-fn (fn A)
+  (let ((rows (array-dimension A 0))
+	(cols (array-dimension A 1)))
+  (declare 
+   ((simple-array single-float) A)
+   (optimize speed)
+   (compiled-function fn))
+    (dotimes (i rows A)
+      (dotimes (j cols)
+	(setf (aref A i j) (funcall fn (aref A i j)))))))
+
+(defun apply-fn2 (fn A B)
+  (let ((rows (array-dimension A 0))
+	(cols (array-dimension A 1)))
+  (declare 
+   ((simple-array single-float) A B)
+   (optimize speed)
+   (compiled-function fn))
+    (dotimes (i rows A)
+      (dotimes (j cols)
+	(setf (aref A i j) (funcall fn (aref A i j) (aref B i j)))))))
+
+(defun float-sigma (x)
+  (declare (single-float x)
+	   (optimize speed))
+  (/ 1s0 (+ 1s0 (exp x))))
+
+(defvar *logistic-y* (apply-fn #'float-sigma *y*))
+
+(defun float-dsigma (diff sigma)
+  (* sigma (- 1s0 sigma) diff))
+
+(defun logistic-regression-iteration (y A x sigma rho)
+  "Iteration step; modifies A."
+  (let ((dy (linear-combination 1s0 (apply-fn #'float-sigma
+					      (times x A))
+				-1s0 y)))
+    (values
+     dy
+     (linear-combination rho A sigma
+			 (times-transposed x (apply-fn2 #'float-sigma dy y)))
+     *true-a*
+     (times-transposed dy dy))))
+
+
