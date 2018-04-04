ad6029a4d495c17e2bf61c14bb1818112c7a996a
Tomas Zellerin <zellerin@gmail.com>
2018-04-04 20:36:32 +0200

    Generalize functions in example 1.

--- regression-test.lisp
+++ regression-test.lisp
@@ -1,10 +1,12 @@
 (in-package regression)
 
-(defun read-comma-file ()
-  (with-open-file (in  "~/src/machine-learning-course/ex1/ex1data1.txt")
+(defun read-comma-file (file)
+  (with-open-file (in  file)
     (loop
       with *readtable* = (copy-readtable)
-	initially (set-macro-character #\, (lambda (s c) (read s)) nil)
+	initially (set-macro-character
+		   #\,
+		   (lambda (s c) (declare (ignore c)) (read s)) nil)
       for line = (read-line in nil nil)
       for size from 0
       while line
@@ -13,19 +15,21 @@
       collect y into yses
       finally (return (values size xses yses)))))
 
-(defun example-1 (count)
   (multiple-value-bind (size xlist ylist)
-      (read-comma-file)
-    (let ((x (make-array `(,size 2) :initial-element 0s0 :element-type 'single-float))
-	  (y (make-array `(,size 1) :initial-element 0s0 :element-type 'single-float))
-	  (A (make-random-array 2 1 1s-4)))
-      (loop for row from 0
-	    for xval in xlist
-	    and yval in ylist
-	    do
-	       (setf (aref x row 0) 1s0
-		     (aref x row 1) xval
-		     (aref y row 0) (coerce yval 'single-float)))
-      (dotimes (i count)
-	(print (linear-regression-iteration y A x -1.25e-4 1s0)))
-      A)))
+      (read-comma-file))
+
+
+(defun get-lr-coefficients (count size xlist ylist)
+  (let ((x (make-array `(,size 2) :initial-element 0s0 :element-type 'single-float))
+	(y (make-array `(,size 1) :initial-element 0s0 :element-type 'single-float))
+	(A (make-random-array 2 1 1s-4)))
+    (loop for row from 0
+	  for xval in xlist
+	  and yval in ylist
+	  do
+	     (setf (aref x row 0) 1s0
+		   (aref x row 1) xval
+		   (aref y row 0) (coerce yval 'single-float)))
+    (dotimes (i count)
+      (print (linear-regression-iteration y A x -1.25e-4 1s0)))
+    (list (aref A 0 0) (aref A 1 0))))
