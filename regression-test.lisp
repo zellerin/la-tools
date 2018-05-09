f697a43c7a767ec1d40da0a29cb6a18eb1e0cd82
Tomas Zellerin <zellerin@gmail.com>
2018-05-09 23:41:23 +0200

    More unification of linear and logistic regression.

--- regression-test.lisp
+++ regression-test.lisp
@@ -18,9 +18,8 @@
 		    with x = (make-array
 			      `(,size (1+
 				       (length (car xses))))
-			      :initial-element 0s0 :element-type 'single-float) 
+			      :initial-element 0s0 :element-type 'single-float)
 		    and y = (make-array `(,size 1) :initial-element 0s0 :element-type 'single-float)
-		    and A = (make-random-array (1+ (length (car xses))) 1 2s-2)
 		    for row from 0
 		    for xrow in xses
 		    and yval in yses
@@ -30,25 +29,7 @@
 			     do
 				(setf (aref x row col)
 				      (coerce xval 'single-float)))
-		       
+
 		       (setf (aref x row 0) 1s0
 			     (aref y row 0) (coerce yval 'single-float))
-		    finally (return (values y A x)))))))
-
-(defun get-lr-coefficients (count y A raw-x &key (sigma (- (/ 1s0 (array-dimension y 0)))) (rho 1s0))
-  (multiple-value-bind (x norm) (normalize raw-x)
-    (dotimes (i count)
-      (print (linear-regression-iteration y A x sigma rho)))
-    (make-array (array-dimension A 0)
-		:element-type 'single-float
-		:displaced-to (times norm A))))
-
-(defun get-log-coefficients (count y A raw-x &key
-					       (sigma (- (/ 1s0 (array-dimension y 0))))
-					       (rho 1s0))
-  (multiple-value-bind (x norm) (normalize raw-x)
-    (dotimes (i count)
-      (print (logistic-regression-iteration y A x sigma rho)))
-    (make-array (array-dimension A 0)
-		:element-type 'single-float
-		:displaced-to (times norm A))))
+		    finally (return (values y x)))))))
--- regression.lisp
+++ regression.lisp
@@ -11,58 +11,77 @@
 (defun M- (a b)
   (linear-combination 1s0 a -1s0 b))
 
-(defun times-sigma (X A)
+(declaim (inline logistic-estimate linear-estimate
+		 logistic-grad-A linear-grad-A))
+
+(defun logistic-estimate (X A)
   "Logistic regression estimate given coefficients A and independent variables X."
   (apply-fn #'float-sigma (times X A)))
 
-(defun logistic-grad-A (X err Y)
-   (times-transposed X (apply-fn2 #'float-dsigma err Y)))
+(defun linear-estimate (X A)
+  "Logistic regression estimate given coefficients A and independent variables X."
+  (times X A))
 
-(defun l-regression-iteration (Y A X sigma rho estimate-fn grad-A-fn)
-  "Update matrix with linear or logistic regression coeficients to
+(defun logistic-grad-A (X err Y)
+  (times-transposed X (apply-fn2 #'float-dsigma err Y)))
+
+(defun linear-grad-A (X err Y)
+  (declare (ignore Y))
+  (times-transposed X err))
+
+(eval-when (:compile-toplevel)
+  (defvar *pairs* '(estimate grad-a)))
+
+(defmacro define-pair (name pars
+		       docstring-fmt
+		       &body body)
+  `(progn
+     (eval-when (:compile-toplevel)
+       (pushnew ',name *pairs*))
+     ,@(loop for prefix in '("LINEAR" "LOGISTIC")
+	     collect
+	     (labels ((@ (name)
+			(alexandria:symbolicate prefix "-" name))
+		      (flet-list-item (name)
+			`(,name (&rest pars) (apply #',(@ name) pars))))
+	       
+	       `(defun ,(@ name) ,pars
+		  ,(format nil docstring-fmt prefix)
+		  (flet ,(mapcar #'flet-list-item *pairs*)
+		    ,@body))))))
+
+(define-pair regression-iteration (Y A X sigma rho)
+  "Update matrix with ~(~A~) regression coeficients to
 better match observed data.
 
-Takes as parameter 
+Takes as parameter
 - the model data original regression coefficients matrix A, matrix of
   the independent values X, matrix of observed values Y
 - the regression process data: rho below 1s0 prevents overfitting, and
   sigma determines the speed of the regression.
-- the regression model data (functions specific for linear or logistic regression)
 
 Return three values: square of the error vector (difference between
 estimated and provided Y), error vector itself and updated matrix with
 the regression coefficients."
-  (let* ((estimated-Y (funcall estimate-fn X A))
+  (let* ((estimated-Y (estimate X A))
 	 (err (M- estimated-Y y))
-	 (a-diff (funcall grad-A-fn X err estimated-Y)))
+	 (a-diff (grad-A X err estimated-Y)))
     (values (times-transposed err err)
 	    err (linear-update rho A sigma a-diff))))
 
-(defun logistic-regression-iteration (Y A X sigma rho)
-  "See l-regression-iteration for details."
-  (l-regression-iteration Y A X sigma rho #'times-sigma #'logistic-grad-A))
+(define-pair test-case (samples indeps &optional (deps 1))
+    "Make a test case for ~(~A~) regression.
 
-(defun linear-regression-iteration (Y A X sigma rho)
-  "See l-regression-iteration for details."
-  (l-regression-iteration Y A X sigma rho #'times
-			  (lambda (X err A)
-			    (declare (ignore A))
-			    (times-transposed X err))))
+Returns X, Y and initial A as values."
+  (let ((X (make-random-array samples indeps 1s0)))
+    (values X (estimate X (make-random-array indeps deps 1s0))
+	    (make-random-array indeps deps 1s-5))))
 
-(defun check-linear (count &optional (sampling 20))
-  (let ((*test-A* (make-random-array 3 1 1s0)))
-    (dotimes (i count)
-      (let ((F
-	      (linear-regression-iteration *y* *test-A* *x* -6s-3 1s0)))
-	(when (zerop (mod i sampling))
-	  (print (aref F 0 0)))))))
+(defmacro with-test-case ((samples indeps deps) &body body)
+  `(multiple-value-bind (X Y A) (test-case ,samples ,indeps ,deps)
+    ,@body))
 
-(defun check-logistic (count pars)
-  (let ((*test-A* (make-random-array 3 1 1s0)))
-    (dotimes (i count)
-      (print (apply 'logistic-regression-iteration pars)))))
-
-(defun try-sigmas (fn y fixed-A x base rho &optional (count 10))
+(define-pair try-sigmas (y fixed-A x base rho &optional (count 10))
   "Try range of sigmas (two orders) around base."
   (loop for i from -1s0 to 1s0 by 0.1
 	for sigma = (* base (expt 10s0 i))
@@ -70,17 +89,30 @@ the regression coefficients."
 	   (let ((A (copy-array fixed-A)))
 	     (princ sigma)
 	     (dotimes (i count)
-	       (format t " ~10e " (aref (funcall fn y a x sigma rho) 0 0)))
+	       (format t " ~10e " (aref (regression-iteration y a x sigma rho) 0 0)))
 	     (terpri))))
 
-(defun try-sigmas-logistic (&rest pars)
-  "Try range of sigmas (two orders) around base for logistic regression."
-  (apply 'try-sigmas 'logistic-regression-iteration pars))
+(define-pair check-regression (count &optional (sampling 20)
+				   (sigma -6s-3))
+  "Test COUNT rounds of regression on random matrixes"
+  (with-test-case (100 120 1)
+    (try-sigmas Y A X sigma 1s0)
+    (dotimes (i count)
+      (let ((F (regression-iteration Y A X
+				     sigma 1s0)))
+	(when (zerop (mod i sampling))
+	  (print (aref F 0 0)))))))
 
-; (try-sigmas-logistic *logistic-y* *test-A* *x* -0.006 1s0)
+(define-pair get-coefficients (y raw-x &key
+				 (A (make-random-array (array-dimension raw-x 1) 1 2s-2))
+				 (sigma (- (/ 1s0 (array-dimension y 0)))) (rho 1s0))
+    "Get coefficients for ~(~A~) regression from data.
 
-(defun try-sigmas-linear (&rest pars)
-  "Try range of sigmas (two orders) around base for linear regression."
-  (apply 'try-sigmas 'linear-regression-iteration pars))
-
-; (try-sigmas-linear *y* *test-A* *x* -1s-2 1s0)
+Assumes that first row of X is all ones. This corresponds to getting
+linear term, and it is also used for normalization purposes."
+  (multiple-value-bind (x norm) (normalize raw-x)
+    (dotimes (i (array-dimension raw-x 0))
+      (print (regression-iteration y A x sigma rho)))
+    (make-array (array-dimension A 0)
+		:element-type 'single-float
+		:displaced-to (times norm A))))
