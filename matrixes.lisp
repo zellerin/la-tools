04ec0c4aa33b99014bbc3be4dde9c435ee6b790b
Tomas Zellerin <zellerin@gmail.com>
2019-04-08 21:45:34 +0200

    Use classes to pass matrix expression information

--- matrixes.lisp
+++ matrixes.lisp
@@ -4,7 +4,7 @@
   "Default field for the matrix elements.")
 
 (defvar *matrix-optimize*
-  (or nil '(speed (safety 0) (debug 0)))
+  (and nil '(speed (safety 0) (debug 0)))
   "Default optimization for the matrix calculation.")
 
 (defvar *default-declarations*
@@ -37,6 +37,77 @@ Otherwise it denotes name of a variable and follows its size.")
 	  'matrix-error :op op :params params
 	  :sizes (list s1 s2)))
 
+(defclass base-matrix-object ()
+  ())
+
+(defclass expr-with-children ()
+  ((children   :accessor get-children   :initarg :children)
+   (assertions :accessor get-assertions :initarg :assertions))
+  (:default-initargs :children nil :assertions nil))
+
+(defclass expr-object (expr-with-children base-matrix-object)
+  ((sizes      :accessor get-sizes      :initarg :sizes)
+   (expr       :accessor get-expr       :initarg :expr))
+  (:default-initargs))
+
+(defclass scalar-expr-object (expr-with-children)
+  ((type       :accessor get-type       :initarg :type :allocation :class)
+   (expr       :accessor get-expr       :initarg :expr)
+   (sizes      :accessor get-sizes      :initarg :sizes :allocation :class))
+  (:default-initargs :sizes nil))
+
+(defclass matrix-copyable-object (base-matrix-object)
+  ((object       :accessor get-object       :initarg :object :reader get-expr)
+   (row-var-name :accessor get-row-var-name :initarg :row-var-name)
+   (col-var-name :accessor get-col-var-name :initarg :col-var-name))
+  (:default-initargs :row-var-name (gensym "ROW") :col-var-name (gensym "COL")))
+
+(defclass matrix-literal-object (base-matrix-object)
+  ((object       :accessor get-object       :initarg :object :reader get-expr)))
+
+(defgeneric matrix-element-of* (object i j)
+  (:method ((object matrix-copyable-object) i j)
+    `(aref ,(get-object object) ,i ,j))
+  (:method ((object matrix-literal-object) i j)
+    `(aref ,(get-object object) ,i ,j))
+  (:method ((object expr-object) i j)
+    (funcall (get-expr object) i j)))
+
+
+(defun make-expr-object (sizes expr &optional assertions children)
+  (make-instance 'expr-object
+		     :sizes sizes
+		     :expr expr
+		     :assertions assertions
+		     :children children))
+
+(defgeneric get-bindings (object)
+  (:method (object) nil)
+  (:method ((object matrix-copyable-object))
+    (let ((expr (get-object object)))
+      `((,(get-row-var-name object) (array-dimension ,expr 0))
+	(,(get-col-var-name object) (array-dimension ,expr 1)))))
+  (:method :around ((object expr-with-children))
+    (append (reduce 'append (get-children object) :key 'get-bindings)
+	    (call-next-method))))
+
+(defmethod get-sizes ((object matrix-copyable-object))
+  (list (get-row-var-name object) (get-col-var-name object)))
+
+(defmethod get-sizes ((object matrix-literal-object))
+  (array-dimensions (get-object object)))
+
+(defmethod get-assertions ((object matrix-literal-object))
+  nil)
+
+(defgeneric get-assertions (object)
+  (:method (object) nil)
+  (:method :around ((object expr-with-children))
+    (append (reduce 'append (get-children object) :key 'get-assertions)
+	    (call-next-method))))
+
+
+
 (defmacro with-matrixes (expr
 			 &key (declarations *default-declarations*)
 			   (field *matrix-field*)
@@ -49,145 +120,137 @@ Use optional declarations to indicate scalars or matrix sizes."
   (let ((*matrix-zero* (coerce 0 field))
 	(*matrix-field* field)
 	(*default-declarations* declarations))
-    (multiple-value-bind (res-type sizes res-expr assertions bindings)
-	(with-matrixes* declarations expr env)
-      `(let ,bindings
-	 ,@assertions
-	 ,(ecase res-type
-	    (copy res-expr)
-	    (scalar res-expr)
-	    (matrix
+    (let ((result (calculate-matrix-object declarations expr env)))
+      `(let ,(get-bindings result)
+	 ,@ (get-assertions result)
+	 ,(etypecase result
+	    ((or matrix-copyable-object matrix-literal-object) (get-object result))
+	    (scalar-expr-object (get-expr result))
+	    (base-matrix-object
 	     (let ((i (gensym "I"))
 		   (j (gensym "J")))
 	       `(let ((res ,(or target
-			       `(make-array (list ,@sizes) :element-type ',*matrix-field*
-							  :initial-element ,*matrix-zero*))))
+				`(make-array (list ,@ (get-sizes result))
+					     :element-type ',*matrix-field*
+					     :initial-element ,*matrix-zero*))))
 		  (declare (optimize ,@optimize)
 			   ,@(mapcar (lambda (d)
 				       `((simple-array ,*matrix-field* (,(cadr d) ,(caddr d)))
 					 ,(car d)))
 				     (remove-if (lambda (a) (member a '(vector scalar))) declarations
 						:key 'car)))
-		  (dotimes (,i ,(car sizes) res)
-		    (dotimes (,j ,(cadr sizes))
+		  (dotimes (,i ,(car (get-sizes result)) res)
+		    (dotimes (,j ,(cadr (get-sizes result)))
 		      (setf (aref res ,i ,j)
-			    ,(funcall res-expr i j))))))))))))
+			    ,(funcall (get-expr result) i j))))))))))))
 
-(defun handle-summation (declarations expr env op)
-  (cond
-    ((eq (with-matrixes* declarations (car expr) env) 'scalar)
-     (error "Summation of scalars not set yet"))
-    (t (handle-map declarations `(,op ,@expr) env))))
+(defgeneric handle-multiply* (a b)
+  (:method (a b) nil)
+  (:method (a (b (eql nil))) a)
+  (:method ((a scalar-expr-object) (b scalar-expr-object))
+    (make-instance 'scalar-expr-object
+		   :expr `(* ,(get-expr a) ,(get-expr b))
+		   :children (list a b)))
+  (:method ((a scalar-expr-object) (b base-matrix-object))
+    (make-expr-object (get-sizes b)
+		      (lambda (i j)
+			`(* ,(get-expr a)
+			    ,(matrix-element-of* b i j)))
+		      nil (list a b)))
+  (:method ((a base-matrix-object) (b base-matrix-object))
+    (let ((a-sizes (get-sizes a))
+	  (b-sizes (get-sizes b)))
+      (make-expr-object `(,(car a-sizes) ,(cadr b-sizes))
+			(lambda (i j)
+			  (let ((k (gensym "K"))
+				(res (gensym "SUM")))
+			    `(let ((,res ,*matrix-zero*))
+			       (declare (,*matrix-field* ,res))
+			       (dotimes (,k ,(cadr a-sizes) ,res)
+				 (incf ,res (* ,(matrix-element-of* a i k)
+					       ,(matrix-element-of* b k j)))))))
+			`((assert (= ,(cadr a-sizes) ,(car b-sizes))
+				  ()
+				  'matrix-error :op "multiplication"
+				  (list ,a ,b)))
+			(list a b)))))
 
 (defun handle-multiply (declarations expr env)
-  (multiple-value-bind (a-res-type a-sizes a-res-expr a-assertions a-bindings)
-      (with-matrixes* declarations (car expr) env)
-    (unless (cdr expr)
-      (return-from handle-multiply
-	   (values a-res-type a-sizes a-res-expr a-assertions a-bindings)))
-       (multiple-value-bind (b-res-type b-sizes b-res-expr b-assertions b-bindings)
-	   (with-matrixes* declarations `(* ,@(cdr expr)) env)
-	 (cond
-	   ((and (eq 'scalar a-res-type)
-		 (eq 'scalar b-res-type))
-	    (values 'scalar nil `(* ,a-res-expr ,b-res-expr)
-		    (append a-assertions b-assertions)))
-	   ((eq 'scalar a-res-type)
-	    (values 'matrix b-sizes
-		    (lambda (i j)
-		      (if (eq b-res-type 'copy) `(* ,a-res-expr (aref ,b-res-expr ,i ,j))
-			  `(* ,a-res-expr ,(funcall  b-res-expr i j))))
-		    (append a-assertions b-assertions)
-		    (append a-bindings b-bindings)))
-	   (t
-	    (values 'matrix `(,(car a-sizes) ,(cadr b-sizes))
-		    (lambda (i j)
-		      (let ((k (gensym "K"))
-			    (res (gensym "SUM")))
-			`(let ((,res ,*matrix-zero*))
-			   (declare (,*matrix-field* ,res))
-			   (dotimes (,k ,(cadr a-sizes) ,res)
-			     (incf ,res (* ,(matrix-element-of a-res-type a-res-expr i k)
-					   ,(matrix-element-of b-res-type b-res-expr k j)))))))
-		    (append a-assertions b-assertions
-			    `((assert (= ,(cadr a-sizes) ,(car b-sizes))
-				      ()
-				      'matrix-error :op "multiplication"
-				      (list ',(car expr) ',(cdr expr)))))
-		    (append a-bindings b-bindings)))))))
+  (let ((a-obj (calculate-matrix-object declarations (car expr) env))
+	(b-obj (and (cdr expr) (calculate-matrix-object declarations `(* ,@(cdr expr)) env))))
+    (handle-multiply* a-obj b-obj)))
 
 (defun handle-trace (declarations expr env)
-  (multiple-value-bind (res-type sizes res-expr assertions bindings)
-      (with-matrixes* declarations (car expr) env)
-    (assert (null (cdr expr)))
-    (cond
-      ((eq 'scalar res-type)
-       (values 'scalar nil res-expr assertions))
-      (t
-       (values 'scalar nil
-	       (let ((k (gensym "K"))
-		     (res (gensym "SUM")))
-		 `(let ((,res ,*matrix-zero*))
-		    (declare (,*matrix-field* ,res))
-		    (dotimes (,k ,(car sizes) ,res)
-		      (incf ,res (* ,(matrix-element-of res-type res-expr k k))))))
-	       (append assertions
-		       `((assert (= ,(cadr sizes) ,(car sizes))
-				 ()
-				 "Trace on non-square: ~s" ',sizes )))
-	       bindings)))))
+  (assert (null (cdr expr)))
+  (let* ((par-object (calculate-matrix-object declarations (car expr) env))
+	 (sizes (get-sizes par-object)))
+    (etypecase par-object
+      (scalar-expr-object par-object)
+      (base-matrix-object
+       (make-instance 'scalar-expr-object
+		    :expr (let ((k (gensym "K"))
+				(res (gensym "SUM")))
+			    `(let ((,res ,*matrix-zero*))
+			       (declare (,*matrix-field* ,res))
+			       (dotimes (,k ,(car sizes) ,res)
+				 (incf ,res (* ,(matrix-element-of* par-object k k))))))
+		    :assertions `((assert (= ,(cadr sizes) ,(car sizes))
+					  ()
+					  "Trace on non-square: ~s" ',sizes ))
+		    :children (list par-object))))))
+
+(defvar *testob*)
 
 (defun handle-map (declarations expr env)
-  (multiple-value-bind (res-type sizes res-expr assertions bindings)
-      (with-matrixes* declarations (cadr expr) env)
-    (cond
-      ((eq 'scalar res-type)
+  (let* ((first-par
+	   (calculate-matrix-object declarations (cadr expr) env))
+	 (sizes (get-sizes first-par)))
+    (etypecase first-par
+      (scalar-expr-object
        (error 'matrix-error :op "map" :params expr))
-      (t
-       (loop for more-args in (cddr expr)
-	     for (new-res-type new-sizes new-res-expr new-assertions new-bindings)
-	       = (multiple-value-call 'list (with-matrixes* declarations more-args env))
-	     append new-bindings into all-bindings
-	     append (append new-assertions
-			    `((assert-compatible-size ,(car new-sizes) ,(car sizes)
-						      ',(car expr) ',(cdr expr))
-			      (assert-compatible-size ,(cadr new-sizes) ,(cadr sizes)
-						      ',(car expr) ',(cdr expr))))
-	     into all-assertions
+      (base-matrix-object
+       (loop with more-args-objs = (mapcar (lambda (a)
+					     (calculate-matrix-object declarations a env))
+					   (cddr expr))
+	     for more-args in more-args-objs
+	     append
+	     `((assert-compatible-size ,(car (get-sizes  more-args))
+				       ,(car sizes)
+				       ',(car expr) ',(cdr expr))
+	       (assert-compatible-size ,(cadr (get-sizes more-args)) ,(cadr sizes)
+				       ',(car expr) ',(cdr expr)))
+	       into all-assertions
 	     do
-		(assert (not (equalp 'scalar new-res-type)))
-	     collect new-res-expr into exprs
-	     collect new-res-type into res-types
-	     finally (return (values 'matrix
-				     sizes
-				     (lambda (i j)
-				       `(,(car expr)
-					 ,(matrix-element-of res-type res-expr i j)
-					 ,@(mapcar (lambda (type expr)
-						     (matrix-element-of type expr i j))
-						   res-types exprs)))
-				     (append assertions all-assertions)
-				     (append bindings all-bindings))))))))
+		(assert (typep more-args 'base-matrix-object))
+	     finally (return (make-expr-object sizes
+					       (lambda (i j)
+						 `(,(car expr)
+						   ,(matrix-element-of* first-par i j)
+						   ,@(mapcar (lambda (obj)
+							       (matrix-element-of* obj i j))
+							     more-args-objs)))
+					       all-assertions
+					       (cons first-par more-args-objs))))))))
 
 (defun handle-diag (declarations expr env)
   (declare (ignore declarations env))
-  (values 'matrix (list (length expr) (length expr))
+  (make-expr-object (list (length expr) (length expr))
 	  (lambda (i j)
 	    `(if (= ,i ,j) (elt ,(apply 'vector  expr) ,i) ,(coerce 0 *matrix-field*)))))
 
-
 (defun handle-transpose (declarations expr env)
-  (multiple-value-bind (res-type sizes res-expr assertions bindings)
-      (with-matrixes* declarations (car expr) env)
-    (assert (null (cdr expr)))
-    (cond
-      ((eq 'scalar res-type)
-       (values 'scalar nil res-expr assertions bindings))
-      (t
-       (values 'matrix (reverse sizes)
-	       (lambda (i j)
-		 (matrix-element-of res-type res-expr j i))
-	       assertions bindings)))))
+  (assert (null (cdr expr)))
+  (let ((result (calculate-matrix-object declarations (car expr) env)))
+    (etypecase result
+      (scalar-expr-object expr)
+      (base-matrix-object
+       (make-expr-object (reverse (get-sizes result))
+			 (lambda (i j)
+			   (matrix-element-of* result j i))
+			 nil (list result))))))
+
+(defun handle-summation (declarations expr env op)
+  (handle-map declarations `(,op ,@expr) env))
 
 (defvar *with-matrixes-handlers*
   '((+ handle-summation +)
@@ -205,41 +268,32 @@ Use optional declarations to indicate scalars or matrix sizes."
      (pushnew (list ',symbol ',name) *with-matrixes-handlers* :key 'car)))
 
 (define-matrix-handler aref handle-aref
-  (values 'copy `((array-dimension (aref ,@expr) 0)
-		  (array-dimension (aref ,@expr) 1))
-	  `(aref ,@expr)))
+  (declare (ignore env declarations))
+  (make-instance 'matrix-copyable-object
+		 :object `(aref ,@expr)))
 
-(defun with-matrixes* (declarations expr env)
+(defun calculate-matrix-object (declarations expr env)
   (when (atom expr)
-    (return-from with-matrixes*
+    (return-from calculate-matrix-object
       (cond
 	((or
 	  (vectorp expr)
 	  (and (symbolp expr) (member expr (assoc 'vector declarations))))
 	 (let ((rows (gensym (format nil "ROWS ~(~A~)" expr))))
-	   (values 'matrix `(,rows 1)
+	   (make-expr-object `(,rows 1)
 		   (lambda (a b)
 		     (declare (ignore b))
 		     `(aref ,expr ,a))
 		   nil
 		   `((,rows (length ,expr))))))
-	((arrayp expr) (values 'copy (array-dimensions expr) expr))
+	((arrayp expr)
+	 ;; FIXME: resolve sizes in compile time
+	 (make-instance 'matrix-literal-object :object expr))
 	((or (constantp expr env)
 	     (and (symbolp expr) (member expr (assoc 'scalar declarations))))
-	 (values 'scalar nil expr))
-	(t (let ((rows (gensym (format nil "ROWS ~(~A~)" expr)))
-		 (cols (gensym (format nil "COLS ~(~A~)" expr))))
-	     (values 'copy
-		     (or (cdr (assoc expr declarations))
-			 `(,rows ,cols))
-		     expr
-		     nil
-		     `((,rows (array-dimension ,expr 0))
-		       (,cols  (array-dimension ,expr 1)))))))))
+	 (make-instance 'scalar-expr-object :expr expr))
+	(t
+	 (make-instance 'matrix-copyable-object :object expr)))))
   (let ((handler (assoc (car expr) *with-matrixes-handlers*)))
     (assert handler () "No matrix handle for ~A" (car expr))
     (apply (cadr handler) declarations (cdr expr) env (cddr handler))))
-
-(defun matrix-element-of (type expr i j)
-  (if (eq type 'copy) `(aref ,expr ,i ,j)
-      (funcall expr i j)))
